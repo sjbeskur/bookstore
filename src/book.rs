4aef273a6ed66f218ecbd97860c305a3dade6f91
sjbeskur <sam.beskur@gmail.com>
2019-07-18 22:41:46 -0600

    thinking ...like a rustac#ian

--- src/book.rs
+++ src/book.rs
@@ -1,36 +1,25 @@
 use std::collections::HashMap;
 
 
-#[derive(Copy,Clone)]
-pub struct Book<'a>{
-    pub title: &'a str,
+#[derive(Clone)]
+pub struct Book{
+    pub title: String,
     pub pages: u32
 }
 
 
-
-
 #[derive(Clone)]
-pub struct Shelf<'a>{
+pub struct Shelf{
     pub id: u32,
-    pub books: Option<HashMap<&'a str, Book<'a> >>
+    pub books: Option<HashMap<String, Book >>
 }
 
-impl<'a> Shelf<'a>{
+impl Shelf{
 
     pub fn add_book(&mut self, b: Book){
-        let mut book_map = self.books.unwrap_or(HashMap::new());
-        book_map.insert(b.title, b);
+        let mut book_map = self.books.clone().unwrap();
+        book_map.insert(b.title.clone(),b);
+        self.books = Some(book_map);
     }
-
-/*
-    pub fn add_book(&mut self, book: Book){
-        if let Some(mut book_map) = self.books {
-            book_map.insert(book.title, book);            
-        }
-        self.books = Some(HashMap::new());
-        self.add_book(book);
-    }
-*/
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 mod book;
 
 use book::*;
@@ -8,36 +7,24 @@ fn main() {
     
     let mut shelfs: HashMap<u32,&Shelf> = HashMap::new();
 
-    let b1 = Book { title: "Learn C#",   pages: 100 };
-    let b2 = Book { title: "Learn Java", pages: 150 };
-    let b3 = Book { title: "Learn Rust", pages: 100000 };
+    let b1 = Book { title: "Learn C#".to_string()  , pages: 100 };
+    let b2 = Book { title: "Learn Java".to_string(), pages: 150 };
+    let b3 = Book { title: "Learn Rust".to_string(), pages: 100000 };
 
     let mut empty_shelf = book::Shelf{ id: 1, books: None };
-
-    shelfs.insert(empty_shelf.id, &empty_shelf);
-
-    // This is failed attempt
-    // cannot add books to my shelf ?
-    /*
     empty_shelf.add_book(b1);
     empty_shelf.add_book(b2);
     empty_shelf.add_book(b3);
-    */
+
+    shelfs.insert(empty_shelf.id, &empty_shelf);
 
     let id = 1;
-    let mut shelf1 = shelfs.get_mut(&id).unwrap(); // id of the empty shelf
+    if let Some(shelf) = shelfs.get(&id){
+        let mut s = (*shelf).clone();
 
-    // try without OO style impl
-    if shelf1.books.is_none(){
-        shelf1.books = Some(HashMap::new());
-        
+        if s.books.is_none(){
+            s.books = Some(HashMap::new());
+            shelfs.insert(id, &s);
+        }
     }
-
-
-
-//    if let Some(mut book_map) = shelf.books{
-//        book_map.insert(b1.title.clone(), b1.clone());
-//    }
-    
-
 }
